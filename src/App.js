a2896d3051589c9259dd0e0a82b63e86cee0cbe1
Abbosbek <abek01sulaymonov@gmail.com>
2022-09-23 14:40:01 +0300

    committed

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import TopBar from "./components/top/TopBar";
 import "./app.css";
 import Home from "./pages/home/Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import UserList from "./pages/userList/UserList";
-import User from "./pages/User/User";
-import NewUser from "./pages/newUser/NewUser";
-import Productlist from "./pages/productList/Productlist";
-import Product from "./pages/product/Product";
+// import UserList from "./pages/userList/UserList";
+// import User from "./pages/User/User";
+// import NewUser from "./pages/newUser/NewUser";
+// import Productlist from "./pages/productList/Productlist";
+// import Product from "./pages/product/Product";
 
 function App() {
   return (
@@ -19,24 +19,24 @@ function App() {
           <Route exact path="/">
             <Home />
           </Route>
-          <Route path="/users">
+          {/* <Route path="/users">
             <UserList />
-          </Route>
-          <Route path="/user/:userId">
+          </Route> */}
+          {/* <Route path="/user/:userId">
             <User />
-          </Route>
-          <Route path="/newuser">
+          </Route> */}
+          {/* <Route path="/newuser">
             <NewUser />
-          </Route>
-          <Route path="/products">
+          </Route> */}
+          {/* <Route path="/products">
             <Productlist />
-          </Route>
-          <Route path="/product/:productId">
+          </Route> */}
+          {/* <Route path="/product/:productId">
             <Product />
           </Route>
           <Route path="/newroduct">
             <NewUser />
-          </Route>
+          </Route> */}
         </Switch>
       </div>
     </Router>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,19 +1,24 @@
-import Chart from '../../components/charts/Chart'
-import Features from '../../components/features/Features'
-import './home.css'
-import { userData } from '../../dummyData'
-import WidgetLg from '../../components/widgetLg/WidgetLg'
-import WidgetSm from '../../components/widgetSm/WidgetSm'
+import Chart from "../../components/charts/Chart";
+import Features from "../../components/features/Features";
+import "./home.css";
+import { userData } from "../../dummyData";
+import WidgetLg from "../../components/widgetLg/WidgetLg";
+import WidgetSm from "../../components/widgetSm/WidgetSm";
 
 export default function Home() {
   return (
     <div classNam="home">
       <Features />
-      <Chart data={ userData } title="User Analytics" grid dataKey="Active User" />
+      <Chart
+        data={userData}
+        title="User Analytics"
+        grid
+        dataKey="Active User"
+      />
       <div classNam="homeWidget">
         <WidgetSm />
-        <WidgetLg /> 
+        <WidgetLg />
       </div>
     </div>
-  )
+  );
 }
